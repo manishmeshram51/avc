0e3d52856643ecc09b44a6b83e40e0bd8160cbcc
Brennan Vincent <brennan@umanwizard.com>
2014-01-20 21:42:34 -0800

    Add JSON output for debugging

--- src/lib/PMDCollector.cpp
+++ src/lib/PMDCollector.cpp
@@ -1,4 +1,7 @@
 #include "PMDCollector.h"
+#include <sstream>
+#include <string>
+#include <iostream>
 
 namespace libpagemaker
 {
@@ -25,11 +28,27 @@ void PMDCollector::addPage()
 /* Output functions */
 void PMDCollector::draw(librevenge::RVNGDrawingInterface* /* painter */) const
 {
+  std::cout << getJsonRepresentation() << std::endl;
 }
 
 std::string PMDCollector::getJsonRepresentation() const
 {
-  return "{ }";
+  std::ostringstream output;
+  output << "{\n";
+  output << "\tpages:\n\t[";
+  for (unsigned i = 0; i < m_pages.size(); ++i)
+  {
+    output << "\n\t\t{\n";
+    output << "\t\t\t\theight: " << m_pageHeight.get() << ",\n";
+    output << "\t\t\t\twidth: " << m_pageWidth.get() << "\n\t\t}";
+    if (i + 1 < m_pages.size())
+    {
+      output << ",";
+    }
+  }
+  output << "\n\t]";
+  output << "\n}";
+  return output.str();
 }
 
 }
