a37f8a68ae9777b8999d82b636fe0f2a2c2c36c4
David Tardon <dtardon@redhat.com>
2014-06-19 12:46:57 +0200

    PMDColor is not polymorphic
    Change-Id: I1562fc1219fde79896ec94325b902d4c29826643

--- src/lib/PMDColor.h
+++ src/lib/PMDColor.h
@@ -14,9 +14,8 @@
 namespace libpagemaker
 {
 
-class PMDColor
+struct PMDColor
 {
-public:
   unsigned m_i;
   uint16_t m_red;
   uint16_t m_green;
@@ -25,11 +24,6 @@ public:
   PMDColor(const unsigned i,const uint16_t red,const uint16_t green,const uint16_t blue)
     : m_i(i), m_red(red), m_green(green), m_blue(blue)
   { }
-
-  virtual ~PMDColor()
-  {
-  }
-
 };
 }
 
